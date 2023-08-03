f1d09090f8709bf669c466ee90ba2b2e1ce13cc2
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-08-03 21:09:04 +0600

    date and time problem solved

---
+++ date-time.js
@@ -0,0 +1,11 @@
+// const today = new Date()
+// const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+// const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+// const dateTime = date+' '+time
+// console.log(dateTime)
+
+const today = new Date();
+const date = today.getFullYear() +'-'+(today.getMonth()+1)+'-'+today.getDate();
+const time = today.getHours()+':'+today.getMinutes() +':'+today.getSeconds();
+const dateTime = date + ' ' +time;
+console.log(dateTime);
